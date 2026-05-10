ece6378b73692db037ccd6e6686fcd2a0fe6c918
scgm0 <2682963017@qq.com>
2026-05-10 13:22:20 +0800

    fix: 改用字符串方式指定xSkillGilded补丁目标以避免硬依赖

--- FontSettings/xSkillsGildedCompat.cs
+++ FontSettings/xSkillsGildedCompat.cs
@@ -1,17 +1,16 @@
 using HarmonyLib;
-using xSkillGilded;
 
 namespace FontSettings;
 
 [HarmonyPatchCategory("xSkillGilded")]
 public static class XSkillsGildedCompat {
-	[HarmonyPatch(typeof(VSImGuiFontPatcher), nameof(VSImGuiFontPatcher.StartPre))]
+	[HarmonyPatch("xSkillGilded.VSImGuiFontPatcher", "StartPre")]
 	[HarmonyPrefix]
 	public static bool Prefix1() {
 		return false;
 	}
-	
-	[HarmonyPatch(typeof(VSImGuiFontPostPatcher), nameof(VSImGuiFontPostPatcher.AssetsLoaded))]
+
+	[HarmonyPatch("xSkillGilded.VSImGuiFontPostPatcher", "AssetsLoaded")]
 	[HarmonyPrefix]
 	public static bool Prefix2() {
 		return false;
